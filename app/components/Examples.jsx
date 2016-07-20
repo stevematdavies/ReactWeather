e04ec8bed6711b523fcaba9d393ddc784c289d6f
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 15:09:42 +0300

    Styling main layout

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,13 @@
 const React = require('react');
+const { Link } = require('react-router')
 
 module.exports = () => 
+    
     <div>
-        <h2>Examples.</h2>
-        <p>Welcome to the Examples page.</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+            <li><Link to="/?location=Turku,Finland">Turku, Finland</Link></li>
+            <li><Link to="/?location=London,UK">London, UK</Link></li>
+        </ol>
     </div>
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,4 +1,14 @@
 const React = require('react');
 const Navigation = require('Navigation');
 
-module.exports = props => <div><Navigation />{props.children}</div>
+module.exports = props => 
+    
+        <div>
+            <Navigation />
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    {props.children}
+                </div>
+            </div>
+    
+        </div>
