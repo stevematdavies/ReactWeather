bddbbff3048e5a31da64e59a528878378aaead3d
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 14:08:28 +0300

    Fix for localhost server runs

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 3000
 
 app.use((req, res, next) => {
     
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next()
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect(`http://${req.hostname}${req.url}`)
     } 
     
     else {
-       res.redirect(`http://${req.hostname}${req.url}`)
+      next()
     }
 })
 app.use(express.static('public'))
