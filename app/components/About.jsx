e1a9e345cf2baa3eee183e951a94fbffc180b50c
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 15:24:14 +0300

    Styling the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,10 @@
 const React = require('react');
 
 module.exports = () => 
-    <div>
-        <h2>About.</h2>
-        <p>Welcome to the About page.</p>
+     
+     <div className="text-center">
+        <h1>About</h1>
+        <p>This app was built in learning <a href="https://facebook.github.io/react/">ReactJS</a></p>
+        <p className="callout"><strong>View the code on<a href="https://github.com/stevematdavies/ReactWeather"> Github</a></strong></p>
     </div>
     
