e14e94cd8d2df3eadc9398398d41eab82614f637
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 15:40:38 +0300

    Updating About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,17 @@ module.exports = () =>
      
      <div className="text-center">
         <h1>About</h1>
-        <p>This app was built in learning with <a href="https://facebook.github.io/react/">ReactJS</a>&trade; &amp;
-        <a href="https://webpack.github.io/"> Webpack</a>&trade;</p>
+        <p>
+            This app was built with:
+        </p>
+        <p> 
+            <small>
+                <a href="https://facebook.github.io/react/">ReactJS</a> &trade; | 
+                <a href="https://webpack.github.io/"> Webpack</a> |
+                <a href="http://foundation.zurb.com/"> Foundation</a> | 
+                <a href="http://openweathermap.org/"> OpenWeatherMap</a> &copy;
+            </small>
+        </p>
         <p className="callout"><strong>View the code on<a href="https://github.com/stevematdavies/ReactWeather"> Github</a></strong></p>
     </div>
     
