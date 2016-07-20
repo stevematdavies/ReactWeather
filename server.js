1267126ff4e3a601ca5e9c996bd74253e2447464
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 13:52:37 +0300

    Updating server script to js2015

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use((req, res, next) => {
-    'use strict'
-
-   
+    
     if (req.headers['x-forwarded-proto'] === 'http') {
         next()
     } 
     
     else {
-       res.redirect('http://' + req.hostname + req.url)
+       res.redirect(`http://${req.hostname}${req.url}`)
     }
 })
 app.use(express.static('public'))
