e070512fc9a919f2671b37ff6371cfa169100f22
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 13:46:45 +0300

    Preparing repo for heroku

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static('public'));
-app.listen(3000, () => console.log('app running on http://localhost:3000') )
+const PORT = process.env.PORT || 3000
+
+app.use((req, res, next) => {
+    'use strict'
+
+   
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next()
+    } 
+    
+    else {
+       res.redirect('http://' + req.hostname + req.url)
+    }
+})
+app.use(express.static('public'))
+app.listen(PORT, () => console.log(`app running on http://localhost:${PORT}` ))
