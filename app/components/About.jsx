11a151e43e3e79407585d968e6489f376813d81a
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 15:29:00 +0300

    Styling the About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,8 @@ module.exports = () =>
      
      <div className="text-center">
         <h1>About</h1>
-        <p>This app was built in learning <a href="https://facebook.github.io/react/">ReactJS</a></p>
+        <p>This app was built in learning with <a href="https://facebook.github.io/react/">ReactJS</a>&trade; &amp;
+        <a href="https://webpack.github.io/"> Webpack</a>&trade;</p>
         <p className="callout"><strong>View the code on<a href="https://github.com/stevematdavies/ReactWeather"> Github</a></strong></p>
     </div>
     
