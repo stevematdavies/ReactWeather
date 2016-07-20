9273a53c67cb491571bbb6b0d10abaceb2c57836
Stephen Davies <stephen.davies@ixonos.com>
2016-07-20 14:35:38 +0300

    Removing <hr> tag

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -8,6 +8,5 @@ module.exports = () =>
                  <IndexLink to='/'	    activeClassName="active"  activeStyle={Styles.activeLink} >Get Weather</IndexLink>
                  <Link to='/about'      activeClassName="active"  activeStyle={Styles.activeLink} >About</Link>
                  <Link to='/examples'   activeClassName="active"  activeStyle={Styles.activeLink} >Examples</Link>
-                 <hr />
             </div>
    
